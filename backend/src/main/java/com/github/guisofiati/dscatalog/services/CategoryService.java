6004058ef60baec1c9b572d8d4f5aac91440f4a7
Guilherme Sofiati Fantin <gui_sf@hotmail.com>
2022-07-20 09:38:10 -0300

    DSCatalog. Seeding do banco de dados H2

--- backend/src/main/java/com/github/guisofiati/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/github/guisofiati/dscatalog/services/CategoryService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.github.guisofiati.dscatalog.entities.Category;
 import com.github.guisofiati.dscatalog.repositories.CategoryRepository;
@@ -14,6 +15,7 @@ public class CategoryService {
 	@Autowired
 	private CategoryRepository repository;
 
+	@Transactional(readOnly = true) // transacao async, ou faz tudo ou n faz nada
 	public List<Category> findAll() {
 		return repository.findAll();
 	}
