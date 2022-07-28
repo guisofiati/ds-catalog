bfb30e6517a05feba0c4c6505eaeb79438914a71
Guilherme Sofiati Fantin <gui_sf@hotmail.com>
2022-07-28 11:15:22 -0300

    DSCatalog. Simulando comportamentos do repositorio

--- backend/src/test/java/com/github/guisofiati/dscatalog/services/ProductServiceTests.java
+++ backend/src/test/java/com/github/guisofiati/dscatalog/services/ProductServiceTests.java
@@ -1,17 +1,28 @@
 package com.github.guisofiati.dscatalog.services;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.springframework.dao.DataIntegrityViolationException;
+// import org.mockito.Mockito.doNothing -> Import estatico
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import com.github.guisofiati.dscatalog.entities.Product;
 import com.github.guisofiati.dscatalog.repositories.ProductRepository;
+import com.github.guisofiati.dscatalog.services.exceptions.DatabaseException;
 import com.github.guisofiati.dscatalog.services.exceptions.ResourceNotFoundException;
+import com.github.guisofiati.dscatalog.tests.Factory;
 
 @ExtendWith(SpringExtension.class)
 public class ProductServiceTests {
@@ -24,17 +35,32 @@ public class ProductServiceTests {
 	
 	private long existingId;
 	private long nonExistingId;
+	private long dependentId;
+	private PageImpl<Product> page; // usar esse tipo para os testes 
+	Product product;
 	
 	@BeforeEach()
 	void setUp() throws Exception {
 		existingId = 1L;
-		nonExistingId = 26L;
+		nonExistingId = 3L;
+		dependentId = 2L;
+		product = Factory.createProduct();
+		page = new PageImpl<>(List.of(product));
 		
 		// simular comportamento do repositorio
 		// quando o id existir
 		Mockito.doNothing().when(repository).deleteById(existingId);
 		// quando o id nao existir
 		Mockito.doThrow(EmptyResultDataAccessException.class).when(repository).deleteById(nonExistingId);
+		// deletar um id que tem associacao com outro, deixando o obj sem referencia
+		Mockito.doThrow(DataIntegrityViolationException.class).when(repository).deleteById(dependentId);
+	
+		Mockito.when(repository.findAll((Pageable)ArgumentMatchers.any())).thenReturn(page);
+	
+		Mockito.when(repository.save(ArgumentMatchers.any())).thenReturn(product);
+		
+		Mockito.when(repository.findById(existingId)).thenReturn(Optional.of(product)); // traz o obj
+		Mockito.when(repository.findById(nonExistingId)).thenReturn(Optional.empty()); // traz vazio
 	}
 	
 	@Test
@@ -50,12 +76,22 @@ public class ProductServiceTests {
 	}
 	
 	@Test
-	public void deleteShouldThrowEmptyResultDataAccessExceptionWhenIdDoesNotExists() {
+	public void deleteShouldThrowResourceNotFoundExceptionWhenIdDoesNotExists() {
 		
 		Assertions.assertThrows(ResourceNotFoundException.class, () -> {
 			service.delete(nonExistingId);
 		});
 		
 		Mockito.verify(repository).deleteById(nonExistingId);
+	}
+	
+	@Test
+	public void deleteShouldThrowDatabaseExceptionWhenDependentId() {
+		
+		Assertions.assertThrows(DatabaseException.class, () -> {
+			service.delete(dependentId);
+		});
+		
+		Mockito.verify(repository).deleteById(dependentId);
 	}
 }
