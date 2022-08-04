263d70eeaca1830b185ebe18fcc651702c42772e
Guilherme Sofiati Fantin <gui_sf@hotmail.com>
2022-08-04 11:06:55 -0300

    DSCatalog. Liberando o h2 para o profile de teste

--- backend/src/main/java/com/github/guisofiati/dscatalog/config/ResourceServerConfig.java
+++ backend/src/main/java/com/github/guisofiati/dscatalog/config/ResourceServerConfig.java
@@ -1,7 +1,10 @@
 package com.github.guisofiati.dscatalog.config;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
@@ -14,10 +17,13 @@ import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
 	@Autowired
+	private Environment env; // pegar as envs do app
+	
+	@Autowired
 	private JwtTokenStore tokenStore;
 	
 	// endpoints
-	private static final String[] PUBLIC = {"/oauth/token"};
+	private static final String[] PUBLIC = {"/oauth/token", "/h2-console/**"};
 	
 	private static final String[] OPERATOR_OR_ADMIN = {"/products/**", "/categories/**"};
 	
@@ -32,6 +38,12 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 	// configurar as rotas
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
+		
+		// liberar o h2
+		if (Arrays.asList(env.getActiveProfiles()).contains("test")) {
+			http.headers().frameOptions().disable();
+		}
+		
 		http.authorizeRequests()
 		.antMatchers(PUBLIC).permitAll() // quem estiver acessando as rotas PUBLIC nao precisa de login, liberado para todos
 		.antMatchers(HttpMethod.GET, OPERATOR_OR_ADMIN).permitAll() // libera somente GET nas rotas OPERATOR_OR_ADMIN apenas com os ROLES informados
